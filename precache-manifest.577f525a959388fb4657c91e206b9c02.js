a74cb43a7c50903341edff516df576f47e8e86cf
olgab <olgabresheva7@gmail.com>
2020-06-13 04:20:05 -0700

    Updates

---
+++ precache-manifest.577f525a959388fb4657c91e206b9c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a80e1ec0f4e78daff5032d8b55c2c8c",
+    "url": "/reactasync/index.html"
+  },
+  {
+    "revision": "1002e07dc49e7d5b1d45",
+    "url": "/reactasync/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1002e07dc49e7d5b1d45",
+    "url": "/reactasync/static/js/2.a38d4c4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactasync/static/js/2.a38d4c4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac5eb5cbb7f7f4a5869f",
+    "url": "/reactasync/static/js/main.b34d9984.chunk.js"
+  },
+  {
+    "revision": "7bc0e3cf7871cf8de497",
+    "url": "/reactasync/static/js/runtime-main.418e8c17.js"
+  }
+]);
